ba512857a0a5e751f826a2d97e66a87dc6872c1c
Stefan Nedelcu <stefan.n@gmx.co.uk>
2021-06-27 13:14:26 +0100

    commit on main

--- pages/components/WordInput.tsx
+++ pages/components/WordInput.tsx
@@ -3,7 +3,9 @@ import React, {useState, useEffect} from "react";
 
 export default function WordInput(props) {
     const [char, setChar] = useState(0); // holds number of characters typed
-    const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0); // holds the index of the word the person is on
+    const [startTime, setStartTime] = useState(0);
+    const [timeTaken, setTimeTaken] = useState(0);
     const averageCharactersPerWord = 5;
     let words = props.phrase.split(" ");
     function checkCorrect(event) {
@@ -15,13 +17,27 @@ export default function WordInput(props) {
                 setIndex(index+1);
                 event.target.value = "";
             }
+        } 
+        else if (index == 0 && startTime === 0) {
+            let start = new Date();
+            setStartTime(start.getTime());
         }
     }
     useEffect(() => {
-        if (index == words.length) {
-            alert(`${char} characters typed`)
+        if (index === words.length) {
+            console.log(`${char} characters typed`)
+            let finish = new Date();
+            let difference = new Date();
+            difference.setTime(finish.getTime() - startTime);
+            setTimeTaken(difference.getSeconds());
         }
     }, [index])
+    useEffect(() => {
+        if (timeTaken !== 0) {
+            const wordsTyped = char/averageCharactersPerWord;
+            alert(`${wordsTyped/timeTaken * 60} words per minute`)
+        }
+    }, [timeTaken])
     return (
         <div>
             <input type="text" name="words" id="word" className="word_input" onKeyPress={() => checkCorrect(event)}/>
