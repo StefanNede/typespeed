697c50b5954e41b9666b521d5ea5682d71ab0f73
Stefan Nedelcu <stefan.n@gmx.co.uk>
2021-06-27 17:05:41 +0100

    commit on main

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import Signup from "./components/signup";
+import Signup from "./components/Signup";
 
 export default function Landing() {
     return (
